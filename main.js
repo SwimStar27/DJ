e32a0a5eaae92d345a69a6f98a1a489f614f1fff
SwimStar27 <94149915+SwimStar27@users.noreply.github.com>
2023-03-28 20:37:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -25,6 +25,17 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,200,400);
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scorelw > 0.2){
+    circle(leftwx,leftwy,24);
+    Cheesecake = Number(leftwy);
+    Brownie = floor(Cheesecake);
+    Volume = Brownie/400;
+    document.getElementById("volume").innerHTML="Volume:  " + Volume;
+    song.setVolume(Volume);
+    }
 }
 
 function play(){
@@ -39,6 +50,8 @@ function gotPoses(results){
     leftwx = results[0].pose.leftWrist.x;
     leftwy = results[0].pose.rightWrist.y;
     console.log("Left Wrist X:  " + leftwx + "Left Wrist Y:" + leftwy);
+
+    scorelw = results[0].pose.keypoints[9].score;
 
     rightwx = results[0].pose.rightWrist.x;
     rightwy = results[0].pose.rightWrist.y;
